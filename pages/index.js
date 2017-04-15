5949ede24dd613b2341eeaa07a3e9950b0c32290
vogelino <contact@vogelino.com>
2017-04-15 13:44:40 +0200

    Create sidebar with selectable options

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,73 @@
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-import Header from '../components/presentational/Header';
-import WorkThumbnail from '../components/presentational/WorkThumbnail';
-import page from '../redux/PageWrapper';
-import { normalizeWork } from '../utils/apiUtil/elemenoParser';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Head from 'next/dist/lib/head';
+import layers from '../state/layers';
 
-
-const WorksPage = ({ works }) => (
-	<div className="home-page">
-		<Header />
-		<ul>
-			{works.map((work) =>
-				<WorkThumbnail key={work.slug} {...work} />)}
-		</ul>
-	</div>
+const SidebarOption = ({
+	id,
+	title,
+	active,
+	description,
+	color,
+	onChange,
+}) => (
+	<li
+		className={`sidebar-option ${active ? 'active' : ''}`}
+		id={id}
+		style={{ '--optioncolor': color }}
+		onClick={() => onChange(id)}
+		role="checkbox"
+		aria-checked={active}
+	>
+		<h5 className="sidebar-option-title">{title}</h5>
+		<p className="sidebar-option-description">{description}</p>
+	</li>
 );
 
-WorksPage.propTypes = {
-	works: PropTypes.arrayOf(PropTypes.shape(WorkThumbnail.propTypes)).isRequired,
+SidebarOption.propTypes = {
+	id: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	color: PropTypes.string.isRequired,
+	active: PropTypes.bool.isRequired,
+	description: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
 };
 
-const preloadFunction = (Api) =>
-	Promise.all([Api.getWorks(), Api.getIntroductionText()])
-		.then(([works, introductionText]) => ({ works, introductionText }));
+class Home extends Component {
+	constructor(props) {
+		super(props);
 
-const shouldLoadData = ({ works, introductionText }) =>
-	Object.keys(introductionText).length === 0 || works.length === 0;
+		this.state = { layers };
+	}
+	toggleLayer(toggleId) {
+		this.setState({
+			layers: this.state.layers.map((layer) => ({
+				...layer,
+				active: layer.id === toggleId ? !layer.active : layer.active,
+			})),
+		});
+	}
+	render() {
+		return (
+			<div className="main-container">
+				<Head>
+					<link rel="stylesheet" media="all" href="/static/styles.css" />
+				</Head>
+				<aside className="sidebar">
+					<h3 className="sidebar-title">ZK/U Mapping</h3>
+					<ul className="sidebar-options">
+						{this.state.layers.map((layer) => (
+							<SidebarOption
+								key={layer.id}
+								onChange={(id) => this.toggleLayer(id)}
+								{...layer}
+							/>
+						))}
+					</ul>
+				</aside>
+			</div>
+		);
+	}
+}
 
-const mapStateToProps = ({ works, introductionText }) => ({
-	works: works.map(normalizeWork),
-	introductionText,
-});
-const connectedPage = connect(mapStateToProps)(WorksPage);
-export default page(connectedPage, preloadFunction, shouldLoadData);
-
+export default Home;
